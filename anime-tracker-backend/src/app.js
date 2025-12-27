1bd1fe25dcb4fcec8d45d54655817c0a314a96cc
Casolin <animecaster@gmail.com>
2025-12-27 17:38:23 +0100

    xqc

--- anime-tracker-backend/src/app.js
+++ anime-tracker-backend/src/app.js
@@ -6,7 +6,12 @@ import userAnimeRouter from "./routes/userAnime.routes.js";
 
 export const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
